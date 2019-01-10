ea8db54b70796401cd2b431f83cb99640e893ceb
Madelyn Hilty <madelynhilty@quickenloans.com>
2019-01-10 10:24:40 -0500

    added routing, ability to enter to add item, css polishing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ToDoListComponent } from 'src/components/to-do-list/to-do-list.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'todolist' ,component: ToDoListComponent },
+  { path: '' , component: ToDoListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
